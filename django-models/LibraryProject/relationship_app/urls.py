e8ad60c0a9d034fc1b77492cecc3bc9bb7844162
Chiamaka Uyanna <chiamakauyanna@gmail.com>
2025-09-06 15:20:57 +0100

    Implementing User Authentication in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
+from . import views
 
 urlpatterns = [
     path("books/", list_books, name="list_books"),  # function-based
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),  # class-based
+    
+     # Authentication
+    path("login/", views.CustomLoginView.as_view(), name="login"),
+    path("logout/", views.CustomLogoutView.as_view(), name="logout"),
+    path("register/", views.register, name="register"),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,36 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.views import LoginView, LogoutView
 
+# 🔹 Registration view
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # log in the new user automatically
+            return redirect("list_books")  # redirect to books page
+    else:
+        form = UserCreationForm()
+    return render(request, "relationship_app/register.html", {"form": form})
+
+
+# 🔹 Login view (using Django's built-in LoginView)
+class CustomLoginView(LoginView):
+    template_name = "relationship_app/login.html"
+    
+
+# 🔹 Logout view (using Django's built-in LogoutView)
+class CustomLogoutView(LogoutView):
+    template_name = "relationship_app/logout.html"
+    
+   # Allow GET requests to log out (instead of POST only)
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs) 
 
 # Function-based view: list all books
 def list_books(request):
