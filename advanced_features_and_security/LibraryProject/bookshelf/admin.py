a517d2cb146a47bac6f4ccd7ab98fda2dde6c362
Chiamaka Uyanna <chiamakauyanna@gmail.com>
2025-09-13 15:38:58 +0100

    checker correction _0

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -3,23 +3,36 @@ from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, Book
 
 
-# Extend the default UserAdmin to show custom fields
-@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
+    # Define how fields appear in admin
     model = CustomUser
-    fieldsets = UserAdmin.fieldsets + (
-        (None, {"fields": ("date_of_birth", "profile_photo")}),
+    list_display = ("username", "email", "first_name", "last_name", "is_staff")
+    list_filter = ("is_staff", "is_superuser", "is_active")
+    search_fields = ("username", "email", "first_name", "last_name")
+    ordering = ("username",)
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Personal Info", {"fields": ("first_name", "last_name", "email", "date_of_birth", "profile_photo")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
     )
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        (None, {"fields": ("date_of_birth", "profile_photo")}),
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "email", "password1", "password2", "is_staff", "is_superuser", "is_active")}
+        ),
     )
 
 
-# Customize how Book is displayed
+# Register your models here
+admin.site.register(CustomUser, CustomUserAdmin)
+
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')
-    list_filter = ('author', 'publication_year')
-    search_fields = ('title', 'author')
+    list_display = ("title", "author", "publication_year")
+    list_filter = ("author", "publication_year")
+    search_fields = ("title", "author")
 
 
 admin.site.register(Book, BookAdmin)
