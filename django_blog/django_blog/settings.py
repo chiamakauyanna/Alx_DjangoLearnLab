785759467f7f73d1a0ab9a35b0419a6ab91e8451
Chiamaka Uyanna <chiamakauyanna@gmail.com>
2025-09-30 10:26:35 +0100

    added postgre database

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -73,12 +73,25 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog-db',       # the database name you created
+        'USER': 'Chiamaka',      # your PostgreSQL username
+        'PASSWORD': '1234',  # your PostgreSQL password
+        'HOST': 'localhost',          # or IP address / domain if remote
+        'PORT': '5432',               # default PostgreSQL port
     }
 }
+
 
 
 # Password validation
