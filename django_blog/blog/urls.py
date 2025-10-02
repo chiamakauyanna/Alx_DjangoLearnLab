a7b8f69e7f0306eec233a6bc251b444407c77030
Chiamaka Uyanna <chiamakauyanna@gmail.com>
2025-10-02 14:29:24 +0100

    checker_correction_1

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
-from .views import (  
+from .views import (
     PostListView,
     PostDetailView,
     PostCreateView,
@@ -12,11 +12,11 @@ from .views import (
 app_name = "blog"
 
 urlpatterns = [
-     path("", PostListView.as_view(), name="post-list"),
-    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
-    path("posts/new/", PostCreateView.as_view(), name="post-create"),
-    path("posts/<int:pk>/edit/", PostUpdateView.as_view(), name="post-update"),
-    path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
+    path("", PostListView.as_view(), name="post-list"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("post/new/", PostCreateView.as_view(), name="post-create"),
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
 
     # existing user routes
     path("login/", auth_views.LoginView.as_view(template_name="blog/login.html"), name="login"),
