7970a0c5a186dd6d11863ed8a27d30e0e468b5fa
Chiamaka Uyanna <chiamakauyanna@gmail.com>
2025-10-10 12:41:22 +0100

    Project Setup and User Authentication for a Social Media API

---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,32 @@
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from django.contrib.auth import authenticate
+from rest_framework import generics, permissions
+from django.contrib.auth import get_user_model
+from .serializers import RegisterSerializer, UserSerializer
+from rest_framework.views import APIView
+
+User = get_user_model()
+
+# Create your views here.
+class RegisterView(generics.CreateAPIView):
+  queryset = User.objects.all()
+  serializer_class = RegisterSerializer
+  
+class LoginView(APIView):
+  def post(self, request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    user = authenticate(username=username, password=password)
+    if user:
+      token, created = Token.objects.get_or_create(user=user)
+      return Response({'token': token.key})
+    return Response({'error': 'Invalid credentials'}, status=400)
+  
+
+class ProfileView(generics.RetrieveUpdateAPIView):
+  permission_classes = [permissions.IsAuthenticated]
+  serializer_class = UserSerializer
+  
+  def get_object(self):
+    return self.request.user
